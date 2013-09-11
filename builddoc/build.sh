567c60227302d24229b38a7d410ee47cf792be32
tef <tef@twentygototen.org>
2013-09-11 14:23:19 +0100

    Add code to make LaTeX pdfs

--- builddoc/build.sh
+++ builddoc/build.sh
@@ -9,18 +9,23 @@ mkdir -p $OUT
 
 legal="UK_LEGAL.md"
 
+PANDOC_HTML="pandoc -f markdown_github -t html5 -s --highlight-style pygments -c css/codeclub.css  --section-divs"
+
+PANDOC_PDF="pandoc -f markdown_github -t latex  --latex-engine=pdflatex"
+
+
 for f in ../en-GB/lessons/*/*.md;
 do
 	base=`basename "$f"`
 	output="lesson${base%%.*}.html"
-	cat "$f" "$legal" | pandoc -f markdown_github -t html5  -s --highlight-style pygments \
-		-c css/codeclub.css \
-		 --section-divs \
-		 -o "$OUT/$output"
+	cat "$f" "$legal" | $PANDOC_HTML -o "$OUT/$output"
+	pdf="lesson${base%%.*}.pdf"
+	cat "$f" "$legal" | $PANDOC_PDF  -o "$OUT/$pdf"
+
 done
 
-cat "../en-GB/volunteer resources/resources_and_gotchas.md" |\
-   pandoc -f markdown_github -t html5  -s --highlight-style pygments -c css/codeclub.css --section-divs -o "$OUT/guide.html"
+cat "../en-GB/volunteer resources/resources_and_gotchas.md" | $PANDOC_HTML -o "$OUT/guide.html"
+cat "../en-GB/volunteer resources/resources_and_gotchas.md" | $PANDOC_PDF -o "$OUT/guide.pdf"
 
 cp -r css $OUT
 cp *.svg $OUT
