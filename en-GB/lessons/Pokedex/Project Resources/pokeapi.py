23b9b42913880af0df6771d919487f254c9140c6
Andy Lulham <a.lulham@gmail.com>
2015-03-14 11:50:46 +0000

    Use `json.loads` to load json (rather than `eval`)

--- en-GB/lessons/Pokedex/Project Resources/pokeapi.py
+++ en-GB/lessons/Pokedex/Project Resources/pokeapi.py
@@ -1,18 +1,19 @@
 import io
+import json
 import urllib.request
 from urllib.request import urlopen
 from PIL import Image, ImageTk
 
 #function to get the data for a pokemon
 def getPokemonData(num):
-    data = urllib.request.urlopen("http://pokeapi.co/api/v1/pokemon/"+str(num)).read()
-    pokemonDict = eval(data)
+    data = urllib.request.urlopen("http://pokeapi.co/api/v1/pokemon/"+str(num)).readall()
+    pokemonDict = json.loads(data.decode("UTF-8"))
     return pokemonDict
 
 #function to get the image for a pokemon
 def getPokemonImage(num):
-    data = urllib.request.urlopen("http://pokeapi.co/api/v1/sprite/"+str(num)).read()
-    spriteDict = eval(data)
+    data = urllib.request.urlopen("http://pokeapi.co/api/v1/sprite/"+str(num)).readall()
+    spriteDict = json.loads(data.decode("UTF-8"))
     imgURL = "http://pokeapi.co" + spriteDict["image"]
     image_bytes = urlopen(imgURL).read()
     data_stream = io.BytesIO(image_bytes)
